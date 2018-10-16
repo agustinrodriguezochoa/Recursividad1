9d0a5b407a810efdfeee14ae8d82a1691f7fcbfa
Agustin Rodriguez Ochoa <agus.ro.ocho99@gmail.com>
2018-10-15 22:33:18 -0500

    Initial commit

---
+++ Recursividad1.py
@@ -0,0 +1,20 @@
+__author_ = ' agustin rodriguez ochoa '
+
+
+def factorial(numero):
+    if (numero == 0 or numero == 1):
+        return 1
+    else:
+        return numero * factorial(numero - 1)
+
+
+if __name__ == "__main__":
+    try:
+        num = int(input("De que número quieres saber el factorial? "))
+        if (num < 0):
+            print("El número debe ser mayor o igual a 0")
+        else:
+            print("El factorial de", num, "es", factorial(num))
+
+    except:
+        print("Se espera un número")
